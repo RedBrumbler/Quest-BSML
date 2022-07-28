852f4492e926bacfc2c797fc1224f4375d2d4c12
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-07-29 00:24:54 +0200

    Redo ordering

--- shared/BSML/TypeHandlers/BackgroundableHandler.hpp
+++ shared/BSML/TypeHandlers/BackgroundableHandler.hpp
@@ -6,9 +6,8 @@
 namespace BSML {
     class BackgroundableHandler : public TypeHandler<Backgroundable*> {
         public:
-            BackgroundableHandler() : TypeHandler<Backgroundable*>() {}
-            using T = Backgroundable*;
             using Base = TypeHandler<Backgroundable*>;
+            BackgroundableHandler() : Base() {}
 
             virtual Base::PropMap get_props() const override;
             virtual Base::SetterMap get_setters() const override;
--- shared/BSML/TypeHandlers/TextHandler.hpp
+++ shared/BSML/TypeHandlers/TextHandler.hpp
@@ -6,9 +6,8 @@
 namespace BSML {
     class TextHandler : public TypeHandler<TMPro::TextMeshProUGUI*> {
         public:
-            TextHandler() : TypeHandler<TMPro::TextMeshProUGUI*>() {}
-            using T = TMPro::TextMeshProUGUI*;
             using Base = TypeHandler<TMPro::TextMeshProUGUI*>;
+            TextHandler() : Base() {}
 
             virtual Base::PropMap get_props() const override;
             virtual Base::SetterMap get_setters() const override;
