c381df9f33f0b4af6b190873ff97d0826a3d3922
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-07-29 00:55:50 +0200

    Implement buttonIcon type handler

---
+++ src/BSML/TypeHandlers/ButtonIconHandler.cpp
@@ -0,0 +1,17 @@
+#include "BSML/TypeHandlers/ButtonIconHandler.hpp"
+
+namespace BSML {
+    static ButtonIconHandler buttonIconHandler;
+
+    ButtonIconHandler::Base::PropMap ButtonIconHandler::get_props() const {
+        return {
+            {"icon", {"icon"}}
+        };
+    }
+
+    ButtonIconHandler::Base::SetterMap ButtonIconHandler::get_setters() const {
+        return {
+            {"icon", [](auto component, auto value){ component->SetIcon(value); }}
+        };
+    }
+}
