033a3b5338dcc2b83900c929cbe4b6a30c466e5f
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-07-29 15:40:49 +0200

    Implement SelectableHandler

---
+++ shared/BSML/TypeHandlers/SelectableHandler.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "BSML/TypeHandlers/TypeHandler.hpp"
+#include "UnityEngine/UI/Selectable.hpp"
+
+namespace BSML {
+    class SelectableHandler : public TypeHandler<UnityEngine::UI::Selectable*> {
+        public:
+            using Base = TypeHandler<UnityEngine::UI::Selectable*>;
+            SelectableHandler() : Base() {}
+
+            virtual Base::PropMap get_props() const override;
+            virtual Base::SetterMap get_setters() const override;
+    };
+}
