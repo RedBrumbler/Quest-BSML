ad314521c103551b2a64067f138838d20fd64580
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-07-29 12:39:35 +0200

    Add ImageHandler

---
+++ src/BSML/TypeHandlers/ImageHandler.cpp
@@ -0,0 +1,21 @@
+#include "BSML/TypeHandlers/ImageHandler.hpp"
+#include "Helpers/utilities.hpp"
+
+namespace BSML {
+    static ImageHandler imageHandler{};
+
+    ImageHandler::Base::PropMap ImageHandler::get_props() const {
+        return {
+            {"image", {"source", "src"}},
+            {"preserveAspect", {"preserve-aspect"}},
+            {"imageColor", {"image-color", "img-color"}}
+        };
+    }
+    ImageHandler::Base::SetterMap ImageHandler::get_setters() const {
+        return {
+            {"image",           [](auto component, auto value){ Utilities::SetImage(component, value); }},
+            {"preserveAspect",  [](auto component, auto value){ component->set_preserveAspect(value); }},
+            {"imageColor",      [](auto component, auto value){ component->set_color(value); }}
+        };
+    }
+}
