3131236a63e3ffb4a3a366bcde0cbc1d467bcf3b
raineio <64136899+raineio@users.noreply.github.com>
2022-08-01 15:10:19 +0200

    forgor a file :skull:

--- src/BSML/TypeHandlers/ScrollIndicatorHandler.cpp
+++ src/BSML/TypeHandlers/ScrollIndicatorHandler.cpp
@@ -6,7 +6,13 @@ UnityEngine::UI::Image* GetHandleImage(BSML::ScrollIndicator* indicator) {
     return indicator->get_Handle()->GetComponent<UnityEngine::UI::Image*>();
 }
 
-namespace BSML {
+void TrySetHandleColor(BSML::ScrollIndicator* indicator, const StringParseHelper& htmlColor) {
+    auto v = htmlColor.tryParseColor(); 
+    if (v.has_value())
+    GetHandleImage(indicator)->set_color(v.value());
+}
+
+namespace BSML { 
     static ScrollIndicatorHandler scrollIndicatorHandler{};
 
     ScrollIndicatorHandler::Base::PropMap ScrollIndicatorHandler::get_props() const {
@@ -18,7 +24,7 @@ namespace BSML {
 
     ScrollIndicatorHandler::Base::SetterMap ScrollIndicatorHandler::get_setters() const {
         return {
-            { "handleColor", [](auto component, auto value){ auto v = value.tryParseColor(); if (v.has_value()) component->set_color(v.value()); }},
+            { "handleColor", TrySetHandleColor },
             { "handleImage", [](auto component, auto value){ Utilities::SetImage(GetHandleImage(component), value); }}
         };
     }
