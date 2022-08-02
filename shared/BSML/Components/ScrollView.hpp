5e4eb629a6679cab5d31f0d95cbe3822a7773352
raineio <64136899+raineio@users.noreply.github.com>
2022-08-02 02:07:51 +0200

    final fix, works and builds correctly. no typical quest crash

---
+++ shared/BSML/Components/ScrollView.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "custom-types/shared/macros.hpp"
+#include "HMUI/ScrollView.hpp"
+
+DECLARE_CLASS_CODEGEN(BSML, ScrollView, HMUI::ScrollView,)
---
+++ shared/BSML/Components/ScrollViewContent.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "custom-types/shared/macros.hpp"
+#include "custom-types/shared/coroutine.hpp"
+
+#include "HMUI/ScrollView.hpp"
+
+#include "UnityEngine/MonoBehaviour.hpp"
+
+DECLARE_CLASS_CODEGEN(BSML, ScrollViewContent, UnityEngine::MonoBehaviour,
+    DECLARE_INSTANCE_FIELD(HMUI::ScrollView*, scrollView);
+    DECLARE_INSTANCE_FIELD(bool, dirty);
+
+    DECLARE_INSTANCE_METHOD(void, Start);
+    DECLARE_INSTANCE_METHOD(void, OnEnable);
+    DECLARE_INSTANCE_METHOD(void, OnRectTransformDimensionsChange);
+    DECLARE_INSTANCE_METHOD(void, Update);
+    DECLARE_INSTANCE_METHOD(void, UpdateScrollView);
+
+    public:
+        custom_types::Helpers::Coroutine SetupScrollView();
+
+)
