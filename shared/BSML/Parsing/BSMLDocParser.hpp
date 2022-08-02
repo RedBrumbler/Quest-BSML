af715e61fc34f7beb9bddeb0d4a854ad2dc2ef98
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-02 23:21:29 +0200

    remove parse metods from docparser

--- shared/BSML/Parsing/BSMLDocParser.hpp
+++ shared/BSML/Parsing/BSMLDocParser.hpp
@@ -13,12 +13,7 @@ namespace BSML {
             static void RegisterTag(BSMLNodeParserBase* parser);
             static void UnRegisterTag(BSMLNodeParserBase* parser);
 
-            // only the root BSML tag is a shared ptr, the rest are all regular c pointers.
-            // memory management is done by BSML, so you don't need to worry about it.
-            static  std::shared_ptr<BSMLParserParams> parse(std::string_view str);
-            static  std::shared_ptr<BSMLParserParams> parse(const tinyxml2::XMLDocument& doc);
             static BSMLNodeParserBase* get_parser(std::string alias);
-            
         private:
             using TagNameToParser = std::map<std::string, BSML::BSMLNodeParserBase*>;
             static TagNameToParser tagNameToParser;
