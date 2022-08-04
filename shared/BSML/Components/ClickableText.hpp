73d7869d3d0863cd204d428c6e8fbb2adc9b187d
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-04 18:51:16 +0200

    Initial implementation for vibrations / sound on clickable text & image

--- shared/BSML/Components/ClickableText.hpp
+++ shared/BSML/Components/ClickableText.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
-#include "UnityEngine/Color.hpp"
-#include "HMUI/CurvedTextMeshPro.hpp"
+#include "custom-types/shared/macros.hpp"
 #include "UnityEngine/EventSystems/IPointerClickHandler.hpp"
 #include "UnityEngine/EventSystems/IPointerEnterHandler.hpp"
 #include "UnityEngine/EventSystems/IPointerExitHandler.hpp"
 #include "UnityEngine/EventSystems/PointerEventData.hpp"
-#include "custom-types/shared/macros.hpp"
+#include "UnityEngine/Color.hpp"
+
+#include "GlobalNamespace/HapticFeedbackController.hpp"
+#include "GlobalNamespace/Signal.hpp"
+#include "Libraries/HM/HMLib/VR/HapticPresetSO.hpp"
+#include "HMUI/CurvedTextMeshPro.hpp"
 
 #include "beatsaber-hook/shared/utils/typedefs-wrappers.hpp"
 
@@ -24,6 +28,7 @@ ___DECLARE_TYPE_WRAPPER_INHERITANCE(BSML, ClickableText, Il2CppTypeEnum::IL2CPP_
     DECLARE_INSTANCE_METHOD(void, UpdateHighlight);
     DECLARE_INSTANCE_METHOD(bool, get_isHighlighted);
     DECLARE_INSTANCE_METHOD(void, set_isHighlighted, bool value);
+    DECLARE_INSTANCE_METHOD(void, Vibrate, bool left);
     
     DECLARE_OVERRIDE_METHOD(void, OnPointerClick, il2cpp_utils::il2cpp_type_check::MetadataGetter<&UnityEngine::EventSystems::IPointerClickHandler::OnPointerClick>::get(), UnityEngine::EventSystems::PointerEventData* eventData);
     DECLARE_OVERRIDE_METHOD(void, OnPointerEnter, il2cpp_utils::il2cpp_type_check::MetadataGetter<&UnityEngine::EventSystems::IPointerEnterHandler::OnPointerEnter>::get(), UnityEngine::EventSystems::PointerEventData* eventData);
@@ -32,6 +37,10 @@ ___DECLARE_TYPE_WRAPPER_INHERITANCE(BSML, ClickableText, Il2CppTypeEnum::IL2CPP_
     /* purely here because CurvedTextMeshPro is larger than it says it is */
     DECLARE_INSTANCE_FIELD(void*, padding1);
     DECLARE_INSTANCE_FIELD(void*, padding2);
+
+    DECLARE_INSTANCE_FIELD(GlobalNamespace::Signal*, buttonClickedSignal);
+    DECLARE_INSTANCE_FIELD(GlobalNamespace::HapticFeedbackController*, hapticFeedbackController);
+    DECLARE_INSTANCE_FIELD(Libraries::HM::HMLib::VR::HapticPresetSO*, hapticFeedbackPresetSO);
 
     DECLARE_INSTANCE_FIELD(UnityEngine::Color, highlightColor);
     DECLARE_INSTANCE_FIELD(UnityEngine::Color, defaultColor);
--- src/BSML/Tags/ClickableImageTag.cpp
+++ src/BSML/Tags/ClickableImageTag.cpp
@@ -7,14 +7,25 @@
 #include "UnityEngine/GameObject.hpp"
 #include "UnityEngine/Transform.hpp"
 #include "UnityEngine/RectTransform.hpp"
+#include "UnityEngine/Resources.hpp"
 #include "UnityEngine/UI/LayoutElement.hpp"
 #include "HMUI/ImageView.hpp"
 
+#include "GlobalNamespace/Signal.hpp"
+#include "GlobalNamespace/MenuShockwave.hpp"
+#include "GlobalNamespace/HapticFeedbackController.hpp"
+#include "Libraries/HM/HMLib/VR/HapticPresetSO.hpp"
+
 using namespace UnityEngine;
+using HapticPresetSO = Libraries::HM::HMLib::VR::HapticPresetSO;
 
 namespace BSML {
     static BSMLNodeParser<ClickableImageTag> imageTagParser({"clickable-image", "clickable-img"});
     
+    GlobalNamespace::Signal* imageClickedSignal = nullptr;
+    HapticPresetSO* imageHapticPreset = nullptr;
+    GlobalNamespace::HapticFeedbackController* imageHapticFeedbackController = nullptr;
+
     UnityEngine::GameObject* ClickableImageTag::CreateObject(UnityEngine::Transform* parent) const {
         DEBUG("Creating Image");
         auto gameObject = GameObject::New_ctor("BSMLClickableImage");
@@ -25,6 +36,27 @@ namespace BSML {
         rectTransform->SetParent(parent, false);
         // TODO: maybe use a default placeholder sprite instead? maybe a BSML image
         image->set_sprite(Utilities::ImageResources::GetBlankSprite());
+
+        if (!imageClickedSignal || !Object::IsNativeObjectAlive(imageClickedSignal)) {
+            auto menuShockWave = Resources::FindObjectsOfTypeAll<GlobalNamespace::MenuShockwave*>().FirstOrDefault();
+            imageClickedSignal = menuShockWave ? menuShockWave->buttonClickEvents.LastOrDefault() : nullptr;
+        }
+
+        if (!imageHapticPreset || !Object::IsNativeObjectAlive(imageHapticPreset)) {
+            imageHapticPreset = UnityEngine::ScriptableObject::CreateInstance<HapticPresetSO*>();
+            imageHapticPreset->duration = 0.02f;
+            imageHapticPreset->strength = 1.0f;
+            imageHapticPreset->frequency = 0.2f;
+            Object::DontDestroyOnLoad(imageHapticPreset);
+        }
+        
+        if (!imageHapticFeedbackController || !Object::IsNativeObjectAlive(imageHapticFeedbackController)) {
+            imageHapticFeedbackController = UnityEngine::Object::FindObjectOfType<GlobalNamespace::HapticFeedbackController*>();
+        }
+
+        image->buttonClickedSignal = imageClickedSignal;
+        image->hapticFeedbackController = imageHapticFeedbackController;
+        image->hapticFeedbackPresetSO = imageHapticPreset;
 
         gameObject->AddComponent<UI::LayoutElement*>();
         return gameObject;
