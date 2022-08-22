e3980c5d839ee54efdecb086a511af1db8785f3a
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-22 12:59:24 +0200

    Init on api calls

--- src/BSML.cpp
+++ src/BSML.cpp
@@ -22,6 +22,7 @@ namespace BSML {
 
     namespace Register {
         MenuButton* RegisterMenuButton(std::string_view text, std::string_view hoverHint, std::function<void(void)> onClick) {
+            Init();
             auto btn = MenuButton::Make_new(text, hoverHint, onClick);
             if (RegisterMenuButton(btn)) return btn;
             btn->Finalize();
@@ -29,26 +30,32 @@ namespace BSML {
         }
     
         bool RegisterMenuButton(MenuButton* button) {
+            Init();
             return MenuButtons::get_instance()->Registerbutton(button);
         }
     
         bool UnRegisterMenuButton(MenuButton* button) {
+            Init();
             return MenuButtons::get_instance()->Registerbutton(button);
         }
 
         bool RegisterSettingsMenu(std::string_view name, std::string_view content_key, Il2CppObject* host, bool enableExtraButtons) {
+            Init();
             return BSMLSettings::get_instance()->AddSettingsMenu(name, content_key, host, enableExtraButtons);
         }
 
         bool UnRegisterSettingsMenu(Il2CppObject* host) {
+            Init();
             return BSMLSettings::get_instance()->RemoveSettingsMenu(host);
         }
 
         bool RegisterGameplaySetupTab(std::string_view name, std::string_view content_key, Il2CppObject* host, MenuType menuType) {
+            Init();
             return BSML::GameplaySetup::get_instance()->AddTab(name, content_key, host, menuType);
         }
 
         bool UnRegisterGameplaySetupTab(std::string_view name) {
+            Init();
             return BSML::GameplaySetup::get_instance()->RemoveTab(name);
         }
     }
