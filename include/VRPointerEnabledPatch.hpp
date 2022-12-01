ff8264374fab6842db2817ec83b20403817c47cd
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-12-02 00:49:10 +0100

    Fix floating screen buggy hook for OnEnable event

--- include/VRPointerEnabledPatch.hpp
+++ include/VRPointerEnabledPatch.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "custom-types/shared/macros.hpp"
+#include "UnityEngine/MonoBehaviour.hpp"
 #include "VRUIControls/VRPointer.hpp"
 
-namespace BSML {
-    class VRPointerEnabledPatch {
-        public:
-            static UnorderedEventCallback<VRUIControls::VRPointer*> OnPointerEnabled;
-    };
-}
+DECLARE_CLASS_CODEGEN(BSML, VRPointerEnabledPatch, UnityEngine::MonoBehaviour,
+    DECLARE_INSTANCE_FIELD(VRUIControls::VRPointer*, _vrPointer);
+    DECLARE_INSTANCE_METHOD(void, Awake);
+    DECLARE_INSTANCE_METHOD(void, OnEnable);
+
+    public:
+        static UnorderedEventCallback<VRUIControls::VRPointer*> OnPointerEnabled;
+)
--- src/Hooks/VRPointer_OnEnable.cpp
+++ src/Hooks/VRPointer_OnEnable.cpp
@@ -1,10 +1,28 @@
 #include "hooking.hpp"
+#include "logging.hpp"
 #include "VRPointerEnabledPatch.hpp"
+#include "UnityEngine/GameObject.hpp"
 
-UnorderedEventCallback<VRUIControls::VRPointer*> BSML::VRPointerEnabledPatch::OnPointerEnabled;
+DEFINE_TYPE(BSML, VRPointerEnabledPatch);
 
-MAKE_AUTO_HOOK_MATCH(VRPointer_OnEnable, &VRUIControls::VRPointer::OnEnable, void, VRUIControls::VRPointer* self) {
-    VRPointer_OnEnable(self);
-    if (BSML::VRPointerEnabledPatch::OnPointerEnabled.size() > 0)
-        BSML::VRPointerEnabledPatch::OnPointerEnabled.invoke(self);
+namespace BSML {
+    UnorderedEventCallback<VRUIControls::VRPointer*> VRPointerEnabledPatch::OnPointerEnabled;
+
+    void VRPointerEnabledPatch::Awake() {
+        _vrPointer = GetComponent<VRUIControls::VRPointer*>();
+    }
+
+    void VRPointerEnabledPatch::OnEnable() {
+        if (OnPointerEnabled.size() > 0)
+            OnPointerEnabled.invoke(_vrPointer);
+    }
+}
+
+MAKE_AUTO_HOOK_MATCH(VRPointer_Awake, &::VRUIControls::VRPointer::Awake, void, VRUIControls::VRPointer* self) {
+    VRPointer_Awake(self);
+    DEBUG("VRPointer_Awake");
+    auto pointerEnabledPatch = self->GetComponent<BSML::VRPointerEnabledPatch*>();
+    if (!pointerEnabledPatch) {
+        self->get_gameObject()->AddComponent<BSML::VRPointerEnabledPatch*>();
+    }
 }
