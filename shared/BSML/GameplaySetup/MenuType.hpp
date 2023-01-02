86cde2bdce9f9927b7ca2a888465a95d02b83d2b
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-01-02 18:21:34 +0100

    cleanup & handle enum differently

--- shared/BSML/GameplaySetup/MenuType.hpp
+++ shared/BSML/GameplaySetup/MenuType.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "beatsaber-hook/shared/utils/il2cpp-type-check.hpp"
 namespace BSML {
     enum class MenuType {
         None = 0,
@@ -10,8 +11,16 @@ namespace BSML {
         All = Solo | Online | Campaign | Custom
     };
     bool operator!(const MenuType& type);
-    MenuType operator |(const MenuType& lhs, const MenuType& rhs); 
-    MenuType& operator |=(MenuType& lhs, const MenuType& rhs); 
-    MenuType operator &(const MenuType& lhs, const MenuType& rhs); 
-    MenuType& operator &=(MenuType& lhs, const MenuType& rhs); 
+    MenuType operator |(const MenuType& lhs, const MenuType& rhs);
+    MenuType& operator |=(MenuType& lhs, const MenuType& rhs);
+    MenuType operator &(const MenuType& lhs, const MenuType& rhs);
+    MenuType& operator &=(MenuType& lhs, const MenuType& rhs);
 }
+
+template<>
+struct ::il2cpp_utils::il2cpp_type_check::il2cpp_no_arg_class<BSML::MenuType> {
+    static inline Il2CppClass* get() {
+        return classof(int);
+    }
+};
+static_assert(sizeof(BSML::MenuType) == sizeof(int));
