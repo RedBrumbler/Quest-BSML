402ed29c8e3ce556a71381b1256a7b56a9dc2812
royston thorndycraft <18059207@brookes.ac.uk>
2023-02-05 18:14:05 +0900

    git blame raineio 6 months ago

--- src/BSML/Tags/LeaderboardTag.cpp
+++ src/BSML/Tags/LeaderboardTag.cpp
@@ -13,14 +13,14 @@ using namespace UnityEngine;
 namespace BSML {
     static BSMLNodeParser<LeaderboardTag> leaderboardTagParser({"leaderboard", "custom-leaderboard"});
 
-    GlobalNamespace::LeaderboardTableView* leaderboardTemplate = nullptr;
     UnityEngine::GameObject* LeaderboardTag::CreateObject(UnityEngine::Transform* parent) const {
-        if (leaderboardTemplate == nullptr) {
+        static SafePtrUnity<GlobalNamespace::LeaderboardTableView> leaderboardTemplate;
+        if (!leaderboardTemplate) {
             leaderboardTemplate = UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::LeaderboardTableView*>().First([](auto x) {
                 return x->get_name() == "LeaderboardTableView";
             });
         }
-        auto table = Object::Instantiate(leaderboardTemplate, parent, false);
+        auto table = Object::Instantiate(leaderboardTemplate.ptr(), parent, false);
         table->set_name("BSMLLeaderboard");
         table->cellPrefab->scoreText->set_enableWordWrapping(false);
         table->GetComponent<VRUIControls::VRGraphicRaycaster*>()->physicsRaycaster = Helpers::GetPhysicsRaycasterWithCache();
