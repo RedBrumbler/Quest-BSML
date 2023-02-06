1b228f035105bdddd2d2ec831f0b54a09968863c
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-02-06 13:55:16 +0100

    Add applying of color0 and color1 to backgroundable

--- src/BSML/TypeHandlers/BackgroundableHandler.cpp
+++ src/BSML/TypeHandlers/BackgroundableHandler.cpp
@@ -8,7 +8,9 @@ namespace BSML {
         return {
             {"background", {"bg", "background"}},
             {"backgroundColor", {"bg-color", "background-color"}},
-            {"backgroundAlpha", {"bg-alpha"}}
+            {"backgroundColor0", {"bg-color0", "background-color0"}},
+            {"backgroundColor1", {"bg-color1", "background-color1"}},
+            {"backgroundAlpha", {"bg-alpha", "background-alpha"}}
         };
     }
 
@@ -16,6 +18,8 @@ namespace BSML {
         return {
             {"background", [](auto component, auto value){ component->ApplyBackground(value); }},
             {"backgroundColor", [](auto component, auto value){ component->ApplyColor(value); }},
+            {"backgroundColor0", [](auto component, auto value){ component->ApplyColor0(value); }},
+            {"backgroundColor1", [](auto component, auto value){ component->ApplyColor1(value); }},
             {"backgroundAlpha", [](auto component, auto value){ component->ApplyAlpha(value); }}
         };
     }
