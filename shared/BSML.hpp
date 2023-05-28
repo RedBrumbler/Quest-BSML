2465f6c9e1fb281e6eb9e51bcaebf87ddd83f7ef
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2023-05-28 16:13:09 +0200

    Fix method declaration

--- shared/BSML.hpp
+++ shared/BSML.hpp
@@ -107,11 +107,11 @@ namespace BSML {
         bool RegisterGameplaySetupTab(std::string_view name, std::string_view content_key, Il2CppObject* host, MenuType menuType = MenuType::All);
 
         /// @brief register a tab for the gameplay setup menu
-        /// @param name the name to display
         /// @param csType C# type of your component you want to register, should inherit MonoBehaviour!
+        /// @param name the name to display
         /// @param menuType where your tab should be displayed. Default is All
         /// @return true if successful, false if failed
-        bool RegisterGameplaySetupTab(std::string_view name, System::Type* csType, MenuType menuType = MenuType::All);
+        bool RegisterGameplaySetupTab(System::Type* csType, std::string_view name, MenuType menuType = MenuType::All);
 
         /// @brief concept to ensure the passed type has a void DidActivate(bool)
         template<typename T>
