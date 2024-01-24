6aaab6b345db9850a14b13599d0a5e0c81d5ff24
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-01-24 16:43:39 +0100

    Add catchwrapper to auto hooking

--- include/hooking.hpp
+++ include/hooking.hpp
@@ -51,7 +51,7 @@ public:
         static const MethodInfo* getInfo() { return ::il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::methodInfo(); }                        \
         static funcType* trampoline() { return &name_; }                                                                                            \
         static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                                                       \
-        static funcType hook() { return hook_##name_; }                                                                                             \
+        static funcType hook() { return &::Hooking::HookCatchWrapper<&hook_##name_, funcType>::wrapper; }                                           \
         static retval hook_##name_(__VA_ARGS__);                                                                                                    \
     };                                                                                                                                              \
     AUTO_INSTALL(name_)                                                                                                                             \
@@ -66,7 +66,7 @@ public:
         static const MethodInfo* getInfo() { return ::il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::methodInfo(); }                        \
         static funcType* trampoline() { return &name_; }                                                                                            \
         static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                                                       \
-        static funcType hook() { return hook_##name_; }                                                                                             \
+        static funcType hook() { return &::Hooking::HookCatchWrapper<&hook_##name_, funcType>::wrapper; }                                           \
         static retval hook_##name_(__VA_ARGS__);                                                                                                    \
     };                                                                                                                                              \
     AUTO_INSTALL_ORIG(name_)                                                                                                                        \
