3276b66b7541987ec4541a5abd31342321ba3f44
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-22 20:42:09 +0100

    Add scheduling after time

--- shared/BSML/MainThreadScheduler.hpp
+++ shared/BSML/MainThreadScheduler.hpp
@@ -11,6 +11,8 @@ DECLARE_CLASS_CODEGEN(BSML, MainThreadScheduler, UnityEngine::MonoBehaviour,
         static std::mutex scheduledMethodsMutex;
         static std::queue<std::function<void()>> nextFrameScheduledMethods;
         static std::mutex nextFrameScheduledMethodsMutex;
+        static std::vector<std::pair<float, std::function<void()>>> scheduledAfterTimeMethods;
+        static std::mutex scheduledAfterTimeMethodsMutex;
 
     public:
         /// @brief schedule a method to be called on the main thread, or if you are on the main thread right now immediately
@@ -27,6 +29,14 @@ DECLARE_CLASS_CODEGEN(BSML, MainThreadScheduler, UnityEngine::MonoBehaviour,
         template<typename T, typename U>
         static void ScheduleNextFrame(T instance, void (U::*method)()) {
             ScheduleNextFrame(std::bind(method, instance));
+        }
+
+        /// @brief schedule a method to be called after time elapses
+        static void ScheduleAfterTime(float time, std::function<void()> method);
+
+        template<typename T, typename U>
+        static void ScheduleAfterTime(float time, T instance, void (U::*method)()) {
+            ScheduleAfterTime(time, std::bind(method, instance));
         }
 
         /// @brief method that checks whether the thread it's called from is the main thread
--- src/BSML/MainThreadScheduler.cpp
+++ src/BSML/MainThreadScheduler.cpp
@@ -1,6 +1,8 @@
 #include "BSML/MainThreadScheduler.hpp"
 #include <mutex>
 
+#include "UnityEngine/Time.hpp"
+
 DEFINE_TYPE(BSML, MainThreadScheduler);
 
 namespace BSML {
@@ -8,6 +10,8 @@ namespace BSML {
     std::mutex MainThreadScheduler::scheduledMethodsMutex;
     std::queue<std::function<void()>> MainThreadScheduler::nextFrameScheduledMethods;
     std::mutex MainThreadScheduler::nextFrameScheduledMethodsMutex;
+    std::vector<std::pair<float, std::function<void()>>> MainThreadScheduler::scheduledAfterTimeMethods;
+    std::mutex MainThreadScheduler::scheduledAfterTimeMethodsMutex;
 
     bool MainThreadScheduler::CurrentThreadIsMainThread() {
         // unity icall for whether this is the main thread
@@ -31,6 +35,12 @@ namespace BSML {
         nextFrameScheduledMethods.push(method);
     }
 
+    void MainThreadScheduler::ScheduleAfterTime(float time, std::function<void()> method) {
+        std::lock_guard<std::mutex> lock(scheduledAfterTimeMethodsMutex);
+        scheduledAfterTimeMethods.emplace_back(time, method);
+        std::stable_sort(scheduledAfterTimeMethods.begin(), scheduledAfterTimeMethods.end(), [](auto const& a, auto const& b){ return a.first < b.first; });
+    }
+
     void MainThreadScheduler::Update() {
         // acquire lock
         std::lock_guard<std::mutex> scheduledMethodsLock(scheduledMethodsMutex);
@@ -46,6 +56,21 @@ namespace BSML {
         while(!nextFrameScheduledMethods.empty()) {
             scheduledMethods.push(nextFrameScheduledMethods.front());
             nextFrameScheduledMethods.pop();
+        }
+
+        // get delta time to use in decreasing timers
+        auto delta = UnityEngine::Time::get_deltaTime();
+        // aqcuire lock for time scheduled methods
+        std::lock_guard<std::mutex> scheduledAfterTimeLock(scheduledAfterTimeMethodsMutex);
+        // decrease all the timers
+        for(auto& [time, func] : scheduledAfterTimeMethods) {
+            time -= delta;
+        }
+
+        // if a timer hit below 0, we can invoke the method and remove it
+        while(scheduledAfterTimeMethods.front().first <= 0.0f) {
+            scheduledAfterTimeMethods.front().second();
+            scheduledAfterTimeMethods.erase(scheduledAfterTimeMethods.begin());
         }
     }
 }
