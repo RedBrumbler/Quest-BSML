a338c7552ee6e26645b549a1ced6c0c74ed48e22
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-23 13:37:54 +0100

    Add methods for awaiting std::shared_future completion

--- shared/BSML/MainThreadScheduler.hpp
+++ shared/BSML/MainThreadScheduler.hpp
@@ -45,6 +45,57 @@ DECLARE_CLASS_CODEGEN(BSML, MainThreadScheduler, UnityEngine::MonoBehaviour,
         /// @brief schedule a method that waits until the until method returns true
         static void ScheduleUntil(std::function<bool()> until, std::function<void()> method);
 
+        template<typename T>
+        static void AwaitFuture(std::shared_future<T> future, std::function<void()> method) {
+            static auto AwaitFuture = [](std::shared_future<T> future) {
+                return !future.valid() || future.wait_for(std::chrono::nanoseconds(0)) == std::future_status::ready;
+            };
+
+            ScheduleUntil(std::bind(AwaitFuture, future), method);
+        }
+
+        template<typename T, typename U, typename V>
+        requires(std::is_invocable_v<void(U::*)(), V*>)
+        static void AwaitFuture(std::shared_future<T> future, void(U::*method)(), V* instance) {
+            static auto AwaitFuture = [](std::shared_future<T> future) {
+                return !future.valid() || future.wait_for(std::chrono::nanoseconds(0)) == std::future_status::ready;
+            };
+
+            static auto InvokeMethod = [](void(U::*method)(), V* instance){
+                std::invoke(method, instance);
+            };
+
+            ScheduleUntil(std::bind(AwaitFuture, future), std::bind(InvokeMethod, method, instance));
+        }
+
+        template<typename T>
+        requires(!std::is_same_v<T, void>)
+        static void AwaitFuture(std::shared_future<T> future, std::function<void(T)> method) {
+            static auto AwaitFuture = [](std::shared_future<T> future) {
+                return !future.valid() || future.wait_for(std::chrono::nanoseconds(0)) == std::future_status::ready;
+            };
+
+            static auto InvokeMethod = [](std::shared_future<T> future, std::function<void(T)> method){
+                std::invoke(method, std::forward<T>(future.get()));
+            };
+
+            ScheduleUntil(std::bind(AwaitFuture, future), std::bind(InvokeMethod, method));
+        }
+
+        template<typename T, typename U, typename V>
+        requires(std::is_invocable_v<void(U::*)(T), V*, T> && !std::is_same_v<T, void>)
+        static void AwaitFuture(std::shared_future<T> future, void(U::* method)(T), V* instance) {
+            static auto AwaitFuture = [](std::shared_future<T> future) {
+                return !future.valid() || future.wait_for(std::chrono::nanoseconds(0)) == std::future_status::ready;
+            };
+
+            static auto InvokeMethod = [](std::shared_future<T> future, void(U::* method)(T), V* instance){
+                std::invoke(method, std::forward<T>(future.get()), instance);
+            };
+
+            ScheduleUntil(std::bind(AwaitFuture, future), std::bind(InvokeMethod, method, instance));
+        }
+
         /// @brief method that checks whether the thread it's called from is the main thread
         static bool CurrentThreadIsMainThread();
 
