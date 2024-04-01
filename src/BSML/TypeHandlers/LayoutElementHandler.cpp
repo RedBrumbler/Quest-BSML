7499c8ddc88ac6fc917ce1e7cc9ae6a0739a7926
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-01 14:54:40 +0200

    Add flex sizes

--- src/BSML/TypeHandlers/LayoutElementHandler.cpp
+++ src/BSML/TypeHandlers/LayoutElementHandler.cpp
@@ -8,6 +8,8 @@ namespace BSML {
             {"ignoreLayout", {"ignore-layout"}},
             {"preferredWidth", {"preferred-width", "pref-width"}},
             {"preferredHeight", {"preferred-height", "pref-height"}},
+            {"flexibleWidth", {"flexible-width", "flex-width"}},
+            {"flexibleHeight", {"flexible-height", "flex-height"}},
             {"minHeight", {"min-height"}},
             {"minWidth", {"min-width"}}
         };
@@ -18,6 +20,8 @@ namespace BSML {
             {"ignoreLayout", [](auto component, auto value){ component->set_ignoreLayout(value); }},
             {"preferredWidth", [](auto component, auto value){ component->set_preferredWidth(value); }},
             {"preferredHeight", [](auto component, auto value){ component->set_preferredHeight(value); }},
+            {"flexibleWidth", [](auto component, auto value){ component->set_flexibleWidth(value); }},
+            {"flexibleHeight", [](auto component, auto value){ component->set_flexibleHeight(value); }},
             {"minHeight", [](auto component, auto value){ component->set_minHeight(value); }},
             {"minWidth", [](auto component, auto value){ component->set_minWidth(value); }}
         };
