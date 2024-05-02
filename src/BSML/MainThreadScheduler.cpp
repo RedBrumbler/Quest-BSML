1aa0e8cca086ed8c98868efa0347cf49f84431be
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-05-02 23:25:34 +0200

    Rework internals so locks are released during execution of the various scheduled methods, so scheduling more methods in scheduled methods should work

--- src/BSML/MainThreadScheduler.cpp
+++ src/BSML/MainThreadScheduler.cpp
@@ -28,86 +28,106 @@ namespace BSML {
             return;
         }
 
-        std::lock_guard<std::mutex> lock(scheduledMethodsMutex);
+        std::unique_lock lock(scheduledMethodsMutex);
         scheduledMethods.push(method);
     }
 
     void MainThreadScheduler::ScheduleNextFrame(std::function<void()> method) {
-        std::lock_guard<std::mutex> lock(nextFrameScheduledMethodsMutex);
+        std::unique_lock lock(nextFrameScheduledMethodsMutex);
         nextFrameScheduledMethods.push(method);
     }
 
     void MainThreadScheduler::ScheduleAfterTime(float time, std::function<void()> method) {
-        std::lock_guard<std::mutex> lock(scheduledAfterTimeMethodsMutex);
+        std::unique_lock lock(scheduledAfterTimeMethodsMutex);
         scheduledAfterTimeMethods.emplace_back(time, method);
         std::stable_sort(scheduledAfterTimeMethods.begin(), scheduledAfterTimeMethods.end(), [](auto const& a, auto const& b){ return a.first < b.first; });
     }
 
     void MainThreadScheduler::ScheduleUntil(std::function<bool()> until, std::function<void()> method) {
-        std::lock_guard<std::mutex> lock(scheduledUntilMethodsMutex);
+        std::unique_lock lock(scheduledUntilMethodsMutex);
         scheduledUntilMethods.emplace_back(false, until, method);
     }
 
     void MainThreadScheduler::Update() {
         {
-            // acquire lock
-            std::lock_guard<std::mutex> scheduledMethodsLock(scheduledMethodsMutex);
-            // run through our backlog
-            while (!scheduledMethods.empty()) {
-                scheduledMethods.front()();
-                scheduledMethods.pop();
+            // aqcuire lock and swap collection to allow unlocking the collection while it's in use
+            std::unique_lock<std::mutex> lock(scheduledUntilMethodsMutex);
+            decltype(scheduledUntilMethods) iterationVector;
+            iterationVector.swap(scheduledUntilMethods);
+            lock.unlock();
+
+            // go through the collection and see if there are any methods we can invoke
+            int invokedCount = 0;
+            for (auto& [willBeInvoked, until, method] : iterationVector) {
+                if (willBeInvoked) continue;
+                if (!until()) continue;
+
+                // we got a method, push it onto the queue of methods that will already be invoked this frame
+                scheduledMethods.emplace(method);
+                willBeInvoked = true;
+                invokedCount++;
             }
 
-            // aqcuire lock
-            std::lock_guard<std::mutex> nextFrameMethodsLock(nextFrameScheduledMethodsMutex);
-            // push all the nextframe methods onto the scheduled methods queue
-            while(!nextFrameScheduledMethods.empty()) {
-                scheduledMethods.push(nextFrameScheduledMethods.front());
-                nextFrameScheduledMethods.pop();
+            if (invokedCount > 0) {
+                // if anything was invoked we need to remove those
+                // lock collection while we are changing its contents
+                lock.lock();
+                scheduledUntilMethods.reserve(iterationVector.size() - invokedCount);
+                for (auto& v : iterationVector) {
+                    // if the first value of the tuple is true, that means it will be invoked and it should not be kept
+                    if (std::get<0>(v)) continue;
+                    scheduledUntilMethods.emplace_back(std::move(v));
+                }
+                lock.unlock();
             }
         }
+
         {
             // get delta time to use in decreasing timers
             auto delta = UnityEngine::Time::get_deltaTime();
+
             // aqcuire lock for time scheduled methods
-            std::lock_guard<std::mutex> scheduledAfterTimeLock(scheduledAfterTimeMethodsMutex);
+            std::unique_lock scheduledAfterTimeLock(scheduledAfterTimeMethodsMutex);
             // decrease all the timers
             for(auto& [time, func] : scheduledAfterTimeMethods) {
                 time -= delta;
             }
 
-            // if a timer hit below 0, we can invoke the method and remove it
+            // if a timer hit below 0, we can add the method to the collection that will be invoked this frame
             while(!scheduledAfterTimeMethods.empty() && scheduledAfterTimeMethods.front().first <= 0.0f) {
-                scheduledAfterTimeMethods.front().second();
+                scheduledMethods.emplace(scheduledAfterTimeMethods.front().second);
                 scheduledAfterTimeMethods.erase(scheduledAfterTimeMethods.begin());
             }
         }
 
         {
+            // acquire lock and swap with empty vector so we can release the lock during execution
+            std::queue<std::function<void()>> methodsToExecute;
+            std::unique_lock scheduledMethodsLock(scheduledMethodsMutex);
+            methodsToExecute.swap(scheduledMethods);
+
+            // make sure that during invoke, the lock is free
+            scheduledMethodsLock.unlock();
+
+            // run through our backlog
+            while (!methodsToExecute.empty()) {
+                methodsToExecute.front()();
+                methodsToExecute.pop();
+            }
+
             // aqcuire lock
-            std::lock_guard<std::mutex> lock(scheduledUntilMethodsMutex);
+            std::queue<std::function<void()>> methodsToExecuteNextFrame;
+            std::unique_lock<std::mutex> nextFrameMethodsLock(nextFrameScheduledMethodsMutex);
+            methodsToExecuteNextFrame.swap(nextFrameScheduledMethods);
+            nextFrameMethodsLock.unlock();
 
-            // go through the collection and see if there are any methods we can invoke
-            bool anyInvoked = false;
-            for (auto& [invoked, until, method] : scheduledUntilMethods) {
-                if (invoked) continue;
-                if (!until()) continue;
-
-                method();
-                invoked = true;
-                anyInvoked = true;
+            scheduledMethodsLock.lock();
+            // push all the nextframe methods onto the scheduled methods queue so they will be executed next frame
+            while(!nextFrameScheduledMethods.empty()) {
+                scheduledMethods.push(methodsToExecuteNextFrame.front());
+                methodsToExecuteNextFrame.pop();
             }
-
-            // if anything was invoked we need to remove those
-            if (anyInvoked) {
-                decltype(scheduledUntilMethods) newVec;
-                newVec.reserve(scheduledUntilMethods.size());
-                for (auto& v : scheduledUntilMethods) {
-                    if (std::get<0>(v)) continue;
-                    newVec.emplace_back(std::move(v));
-                }
-                scheduledUntilMethods = std::move(newVec);
-            }
+            scheduledMethodsLock.unlock();
         }
     }
 }
