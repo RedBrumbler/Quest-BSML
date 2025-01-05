977780ab64bc81d717200d14db400ac9c30632ec
kodenamekrak <kyelodge2005@gmail.com>
2025-01-05 16:57:16 +0000

    Use paper2

--- include/logging.hpp
+++ include/logging.hpp
@@ -4,7 +4,7 @@
 #include "beatsaber-hook/shared/utils/typedefs-string.hpp"
 #include <string_view>
 
-#include "paper/shared/logger.hpp"
+#include "paper2_scotland2/shared/logger.hpp"
 
 template <> struct fmt::formatter<::StringW> : formatter<string_view> {
     // parse is inherited from formatter<string_view>.
